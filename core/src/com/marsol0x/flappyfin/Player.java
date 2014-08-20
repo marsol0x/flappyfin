da2a74231d9a144afdac8a5cc199ba3e91851458
Marshel Helsper <marsol0x@gmail.com>
2014-08-19 23:40:01 -0400

    Created a player object to handle position updates, death status, and player input

---
+++ core/src/com/marsol0x/flappyfin/Player.java
@@ -0,0 +1,54 @@
+package com.marsol0x.flappyfin;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+
+public class Player {
+    private final float SPEED = 125f;
+    private Vector2 playerPos;
+    private float hopDelay = 0f;
+
+    private boolean dead = false;
+
+    public Player (float x, float y) {
+        playerPos = new Vector2(x, y);
+    }
+
+    public void update(float delta) {
+        if (hopDelay > 0) {
+            hopDelay -= delta;
+        }
+
+        if (Gdx.input.justTouched()) {
+            playerPos.y += SPEED / 3f;
+            hopDelay = 0.2f;
+
+            if (playerPos.y > Gdx.graphics.getHeight()) {
+                playerPos.y = Gdx.graphics.getHeight();
+            }
+            return;
+        }
+
+        playerPos.y -= delta * SPEED;
+
+        if (playerPos.y < 0) {
+            dead = true;
+        }
+    }
+
+    public float getX() {
+        return playerPos.x;
+    }
+
+    public float getY() {
+        return playerPos.y;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public void kill() {
+        dead = true;
+    }
+}
