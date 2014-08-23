4da778b8c682568cdf024d81ad66fe28c0bcbbfe
Marshel Helsper <marsol0x@gmail.com>
2014-08-23 02:50:37 -0400

    Adding HTML build support

---
+++ html/src/com/marsol0x/flappyfin/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.marsol0x.flappyfin.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.marsol0x.flappyfin.FlappyFinGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new FlappyFinGame();
+        }
+}
